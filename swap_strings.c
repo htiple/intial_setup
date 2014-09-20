ae4acab476f07d230d7edbc9ef2e5259184be23f
harihar <htiple@gmail.com>
2014-09-20 23:17:26 +0530

    first checkin

---
+++ swap_strings.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void swap(char **s1, char **s2){
+	
+	printf("Value of s1 is : %s\n",s1);
+	printf("Value of s2 is : %s\n",s2);
+	char *tmp;
+	*tmp = *s1;
+	*s1 = *s2;
+	*s2 = *tmp;
+	printf("Value of s1 is : %s\n",s1);
+	printf("Value of s2 is : %s\n",s2);
+}
+int main(){
+
+char *p1,*p2;
+
+p1="My name is harihar";
+p2="My name is tiple";
+
+swap(&p1,&p2);
+	printf("Value of s1 is : %s\n",p1);
+	printf("Value of s2 is : %s\n",p2);
+return 0;
+
+}
