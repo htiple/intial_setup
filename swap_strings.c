25f286f30ec3573e72a0f578bcc659e44bfc15ba
laptop <h_tiple@yahoo.com>
2014-09-21 02:32:48 +0530

    added date

--- swap_strings.c
+++ swap_strings.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 // This code will go int the testing branch
-/* Adding one more comment line in the testing branch */
+/* Adding one more comment line in the testing branch *//*21sep*/
 void swap(char **s1, char **s2){
 	
 	printf("Value of s1 is : %s\n",s1);
